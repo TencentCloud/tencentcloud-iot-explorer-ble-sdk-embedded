45c49ad8167599a700bed953dab8ea1af8caecda
wisonhu <wisonhu@tencent.com>
2022-03-08 15:32:46 +0800

    fix: 1.enum type conversion problems;2.enum Chinese conversion problems.

--- scripts/interpret_json_dt/src/interpret_dt_ble.py
+++ scripts/interpret_json_dt/src/interpret_dt_ble.py
@@ -176,9 +176,9 @@ class iot_object:
                    "\n\tble_qiot_log_d(\"set id {} float value %f\", tmp_float);" \
                    "\n\treturn 0;\n}}\n".format(id)
         elif type == TEMPLATE_CONSTANTS.ENUM:
-            ctx += "\n{{\n\tint tmp_enum = 0;" \
+            ctx += "\n{{\n\tuint16_t tmp_enum = 0;" \
                    "\n\tmemcpy(&tmp_enum, data, sizeof(uint16_t));" \
-                   "\n\ttmp_enum = NTOHL(tmp_enum);" \
+                   "\n\ttmp_enum = NTOHS(tmp_enum);" \
                    "\n\tble_qiot_log_d(\"set id {} int value %d\", tmp_enum);" \
                    "\n\treturn 0;\n}}\n".format(id)
         elif type == TEMPLATE_CONSTANTS.TIMESTAMP:
@@ -217,7 +217,7 @@ class iot_int(iot_object):
         self.max = ctx[TEMPLATE_CONSTANTS.MAX]
         self.start = ctx[TEMPLATE_CONSTANTS.START]
         self.step = ctx[TEMPLATE_CONSTANTS.STEP]
-        self.unit = ctx[TEMPLATE_CONSTANTS.UNIT]
+        # self.unit = ctx[TEMPLATE_CONSTANTS.UNIT]
 
     def get_header_data(self, ctx_format, id="", sub_id=""):
         ctx = ""
@@ -328,7 +328,7 @@ class iot_enum(iot_object):
             ctx += "\n// define {} {} enum".format(id, sub_id)
             ctx += "\nenum {"
             for k in sorted(self.enum.keys()):
-                ctx += "\n\t" + enum_format.format(id.upper(), sub_id.upper(), self.enum[k].upper(), int(k))
+                ctx += "\n\t" + enum_format.format(id.upper(), sub_id.upper(), int(k), int(k))
             enum_format = ctx_format + "_{}_BUTT,"
             ctx += "\n\t" + enum_format.format(id.upper(), sub_id.upper())
             ctx += "\n};\n"
@@ -336,7 +336,7 @@ class iot_enum(iot_object):
             ctx += "\n// define {} enum".format(id)
             ctx += "\nenum {"
             for k in sorted(self.enum.keys()):
-                ctx += "\n\t" + enum_format.format(id.upper(), self.enum[k].upper(), int(k))
+                ctx += "\n\t" + enum_format.format(id.upper(), int(k), int(k))
             enum_format = ctx_format + "_{}_BUTT,"
             ctx += "\n\t" + enum_format.format(id.upper())
             ctx += "\n};\n"
